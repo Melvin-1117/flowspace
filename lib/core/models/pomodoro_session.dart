4b0bde617224598e78faec82117a1d7934f89ba5
Melvimn-1117 <antonymelvin81@gmail.com>
2026-04-30 22:18:35 +0530

    Fresh start

--- lib/core/models/pomodoro_session.dart
+++
@@ -1,9 +0,0 @@
-import 'package:isar/isar.dart';
-
-@collection
-class PomodoroSession {
-  Id id = Isar.autoIncrement;
-  late DateTime date;
-  late int durationMinutes;
-  late bool isCompleted;
-}
